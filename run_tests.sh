bc2362da90e392ebcd5a4e99d243f88a589bea7e
BorisSphere <boris.flores@sphere-itc.com>
2026-08-30 21:43:59 -0500

    14

--- run_tests.sh
+++ run_tests.sh
@@ -1,23 +1,29 @@
-#! /bin/bash
-echo " Iniciando ejecución de pruebas en Jenkins ..."
+#!/bin/bash
+echo "Iniciando ejecución de pruebas en Jenkins ..."
+
 # Verificar si el entorno virtual existe
 if [ ! -d "venv" ]; then
-    echo " Entorno virtual no encontrado. Creándolo..." 
+    echo "Entorno virtual no encontrado. Creándolo..." 
     python3 -m venv venv
 fi
+
 # Activar el entorno virtual correctamente
-if [ -f "venv/bin/activate" ]; then source venv/bin/activate
+if [ -f "venv/bin/activate" ]; then 
+    source venv/bin/activate
 elif [ -f "venv/Scripts/activate" ]; then 
-source venv/Scripts/activate
+    source venv/Scripts/activate
 else
-echo " Error: No se pudo activar el entorno virtual."
-exit 1
+    echo "Error: No se pudo activar el entorno virtual."
+    exit 1
 fi
-# Verificar si 'pip" está instalado correctamente 
-echo "Instalando dependencias... "
-pip install -upgrade pip - break-system-packages 
-pip install -r requirements.txt -break-system-packages
+
+# Instalar dependencias dentro del entorno virtual
+echo "Instalando dependencias..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
 # Ejecutar las pruebas
-echo "s Ejecutando pruebas con pytest ..."
-venv/bin/python -m pytest tests/ --junitxml=reports/test-results.xml --html=reports/report.html  --self-contained-html
+echo "Ejecutando pruebas con pytest ..."
+pytest tests/ --junitxml=reports/test-results.xml --html=reports/report.html --self-contained-html
+
 echo "Pruebas finalizadas. Reportes en reports/"
