28155a2c8d330e577621e3cc9e325c0c476327ce
BorisSphere <boris.flores@sphere-itc.com>
2026-08-30 21:14:35 -0500

    8

---
+++ tests/test_suma.py
@@ -0,0 +1,16 @@
+import pytest
+
+def suma(a,b):
+    """Funcion que suma dos numeros"""
+    return a+b
+
+def test_suma():
+    assert suma(1,2)=3
+    assert suma(3,2)=5
+    assert suma(3,3)=6
+
+def test_suma_fail():
+    assert suma(1,2)=6
+    
+
+
